3f6f69da724862abca38c7322bfcfdd37924ba18
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-10-11 09:29:20 +0800

    Simplified matrixMul

--- matrixMul/matrixMul.cu
+++ matrixMul/matrixMul.cu
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-template <int BLOCK_SIZE> __global__ void matrixMulCUDA(float *C, float *A, float *B, int wA, int wB)
+template <int BLOCK_SIZE> __global__ void matrixMul(float *C, float *A, float *B, int wA, int wB)
 {
 	int bx = blockIdx.x;
 	int by = blockIdx.y;
@@ -30,25 +30,21 @@ template <int BLOCK_SIZE> __global__ void matrixMulCUDA(float *C, float *A, floa
 	C[c + wB * ty + tx] = Csub;
 }
 
-void constantInit(float *data, int size, float val)
-{
-	for (int i = 0; i < size; ++i)
-	{
-		data[i] = val;
-	}
-}
-
 int main(int argc, char **argv)
 {
 	const int block_size = 32;
 	dim3 dimsA(5 * 2 * block_size, 5 * 2 * block_size, 1);
 	dim3 dimsB(5 * 4 * block_size, 5 * 2 * block_size, 1);
+	dim3 dimsC(dimsB.x, dimsA.y, 1);
 	unsigned int size_A = dimsA.x * dimsA.y;
-	unsigned int mem_size_A = sizeof(float) * size_A;
-	float *h_A = (float *)malloc(mem_size_A);
 	unsigned int size_B = dimsB.x * dimsB.y;
+	unsigned int size_C = dimsC.x * dimsC.y;
+	unsigned int mem_size_A = sizeof(float) * size_A;
 	unsigned int mem_size_B = sizeof(float) * size_B;
+	unsigned int mem_size_C = sizeof(float) * size_C;
+	float *h_A = (float *)malloc(mem_size_A);
 	float *h_B = (float *)malloc(mem_size_B);
+	float *h_C = (float *)malloc(mem_size_C);
 	for (int i = 0; i < size_A; ++i)
 	{
 		h_A[i] = 1.0f;
@@ -58,18 +54,15 @@ int main(int argc, char **argv)
 	{
 		h_B[i] = valB;
 	}
-	dim3 dimsC(dimsB.x, dimsA.y, 1);
-	unsigned int mem_size_C = dimsC.x * dimsC.y * sizeof(float);
-	float *h_C = (float *)malloc(mem_size_C);
 	float *d_A, *d_B, *d_C;
 	cudaMalloc((void **)&d_A, mem_size_A);
 	cudaMalloc((void **)&d_B, mem_size_B);
 	cudaMalloc((void **)&d_C, mem_size_C);
 	cudaMemcpy(d_A, h_A, mem_size_A, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_B, h_B, mem_size_B, cudaMemcpyHostToDevice);
-	dim3 threads(block_size, block_size);
-	dim3 grid(dimsB.x / threads.x, dimsA.y / threads.y);
-	matrixMulCUDA<block_size><<<grid, threads>>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
+	dim3 threadsPerBlock(block_size, block_size);
+	dim3 blocksPerGrid(dimsB.x / threadsPerBlock.x, dimsA.y / threadsPerBlock.y);
+	matrixMul<block_size><<<blocksPerGrid, threadsPerBlock>>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
 	cudaDeviceSynchronize();
 
 	cudaEvent_t start;
@@ -78,38 +71,33 @@ int main(int argc, char **argv)
 	cudaEventCreate(&stop);
 	cudaEventRecord(start, 0);
 	int nIter = 300;
-	for (int j = 0; j < nIter; j++)
+	for (int i = 0; i < nIter; ++i)
 	{
-		matrixMulCUDA<block_size><<< grid, threads >>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
+		matrixMul<block_size><<<blocksPerGrid, threadsPerBlock>>>(d_C, d_A, d_B, dimsA.x, dimsB.x);
 	}
 	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop);
 	float msecTotal;
 	cudaEventElapsedTime(&msecTotal, start, stop);
-
 	float msecPerMatrixMul = msecTotal / nIter;
 	double flopsPerMatrixMul = 2.0 * (double)dimsA.x * (double)dimsA.y * (double)dimsB.x;
 	double gigaFlops = (flopsPerMatrixMul * 1.0e-9f) / (msecPerMatrixMul / 1000.0f);
-	printf("Performance= %.2f GFlop/s, Time= %.3f msec, Size= %.0f Ops, WorkgroupSize= %u threads/block\n", gigaFlops, msecPerMatrixMul, flopsPerMatrixMul, threads.x * threads.y);
-	cudaMemcpy(h_C, d_C, mem_size_C, cudaMemcpyDeviceToHost);
+	printf("Performance= %.2f GFlop/s, Time= %.3f msec, Size= %.0f Ops, WorkgroupSize= %u threads/block\n", gigaFlops, msecPerMatrixMul, flopsPerMatrixMul, threadsPerBlock.x * threadsPerBlock.y);
 
-	double eps = 1.e-6 ;
-	for (int i = 0; i < (int)(dimsC.x * dimsC.y); i++)
+	cudaMemcpy(h_C, d_C, mem_size_C, cudaMemcpyDeviceToHost);
+	for (int i = 0; i < dimsC.x * dimsC.y; ++i)
 	{
-		double abs_err = fabs(h_C[i] - (dimsA.x * valB));
-		double dot_length = dimsA.x;
-		double abs_val = fabs(h_C[i]);
-		double rel_err = abs_err/abs_val/dot_length ;
-		if (rel_err > eps)
+		double ref = dimsA.x * valB;
+		if (fabs(h_C[i] - ref) / fabs(h_C[i]) / dimsA.x > 1.e-6)
 		{
-			printf("Error! Matrix[%05d]=%.8f, ref=%.8f error term is > %E\n", i, h_C[i], dimsA.x*valB, eps);
+			printf("Matrix[%05d]=%.8f, ref=%.8f\n", i, h_C[i], ref);
 		}
 	}
-	free(h_A);
-	free(h_B);
-	free(h_C);
 	cudaFree(d_A);
 	cudaFree(d_B);
 	cudaFree(d_C);
 	cudaDeviceReset();
+	free(h_A);
+	free(h_B);
+	free(h_C);
 }
