99e9c6f0851604c8b1642203ff0b17ca5708a810
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-10-13 19:24:13 +0800

    Added comments to vectorAdd.cu

--- vectorAdd/vectorAdd.cu
+++ vectorAdd/vectorAdd.cu
@@ -12,39 +12,59 @@ __global__ void vectorAdd(const float *a, const float *b, float *c, int numEleme
 int main()
 {
 	int numElements = 50000;
-	size_t size = numElements * sizeof(float);
+
+	// Allocate vectors a, b and c in host memory.
+	size_t size = sizeof(float) * numElements;
 	float *h_a = (float *)malloc(size);
 	float *h_b = (float *)malloc(size);
 	float *h_c = (float *)malloc(size);
+
+	// Initialize vectors a and b.
 	for (int i = 0; i < numElements; ++i)
 	{
 		h_a[i] = rand()/(float)RAND_MAX;
 		h_b[i] = rand()/(float)RAND_MAX;
 	}
+
+	// Allocate vectors a, b and c in device memory.
 	float *d_a;
 	float *d_b;
 	float *d_c;
 	cudaMalloc((void **)&d_a, size);
 	cudaMalloc((void **)&d_b, size);
 	cudaMalloc((void **)&d_c, size);
+
+	// Copy vectors a and b from host memory to device memory synchronously.
 	cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
 	cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
+
+	// Determine the number of threads per block and the number of blocks per grid.
 	int threadsPerBlock = 256;
 	int blocksPerGrid = (numElements + threadsPerBlock - 1) / threadsPerBlock;
+
+	// Invoke the kernel on device asynchronously.
 	vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_a, d_b, d_c, numElements);
+
+	// Copy vector c from device memory to host memory synchronously.
 	cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
+
+	// Validate the result.
 	for (int i = 0; i < numElements; ++i)
 	{
-		if (fabs(h_a[i] + h_b[i] - h_c[i]) > 1e-5)
+		float actual = h_c[i];
+		float expected = h_a[i] + h_b[i];
+		if (fabs(actual - expected) > 1e-5)
 		{
-			fprintf(stderr, "Result verification failed at element %d!\n", i);
+			fprintf(stderr, "h_c[%d] = %f, ref = %f\n", i, actual, expected);
 		}
 	}
-	cudaFree(d_a);
-	cudaFree(d_b);
+
+	// Cleanup.
 	cudaFree(d_c);
+	cudaFree(d_b);
+	cudaFree(d_a);
 	cudaDeviceReset();
-	free(h_a);
-	free(h_b);
 	free(h_c);
+	free(h_b);
+	free(h_a);
 }
