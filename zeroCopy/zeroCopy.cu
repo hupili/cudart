08d60fc1e42336f6d06909236bb69c791c2d420a
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-10-13 21:40:33 +0800

    Added comments to zeroCopy.cu

--- zeroCopy/zeroCopy.cu
+++ zeroCopy/zeroCopy.cu
@@ -10,38 +10,47 @@ int main(int argc, char *argv[])
 {
 	const unsigned int lws = 256;
 	const unsigned int gws = 1024 * lws;
+
+	// Allocate pinned vectors a, b and c in host memory with the cudaHostAllocMapped flag.
 	float* h_a;
 	float* h_b;
 	float* h_c;
 	cudaHostAlloc((void**)&h_a, sizeof(float) * gws, cudaHostAllocMapped);
 	cudaHostAlloc((void**)&h_b, sizeof(float) * gws, cudaHostAllocMapped);
 	cudaHostAlloc((void**)&h_c, sizeof(float) * gws, cudaHostAllocMapped);
+
+	// Initialize vectors a and b.
 	for (int i = 0; i < gws; ++i)
 	{
 		h_a[i] = rand() / (float)RAND_MAX;
 		h_b[i] = rand() / (float)RAND_MAX;
 	}
+
+	// Get the mapped device points.
 	float* d_a;
 	float* d_b;
 	float* d_c;
 	cudaHostGetDevicePointer(&d_a, h_a, 0);
 	cudaHostGetDevicePointer(&d_b, h_b, 0);
 	cudaHostGetDevicePointer(&d_c, h_c, 0);
+
+	// Invoke the kernel.
 	zeroCopy<<<gws / lws, lws>>>(d_a, d_b, d_c);
 	cudaDeviceSynchronize();
-	bool passed = true;
+
+	// Validate result.
 	for (int i = 0; i < gws; ++i)
 	{
 		const float ref = h_a[i] + h_b[i];
 		if (fabs(h_c[i] - ref) > 1e-7)
 		{
 			printf("i = %d, ref = %f, h_c[i] = %f\n", i, ref, h_c[i]);
-			passed = false;
 			break;
 		}
 	}
-	printf("zeroCopy %s\n", passed ? "passed" : "failed");
-	cudaFreeHost(h_a);
-	cudaFreeHost(h_b);
+
+	// Cleanup.
 	cudaFreeHost(h_c);
+	cudaFreeHost(h_b);
+	cudaFreeHost(h_a);
 }
