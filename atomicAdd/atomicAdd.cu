fdab41926f1583830316439499a8949285a07676
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-10-14 14:42:31 +0800

    Revised atomicAdd

--- atomicAdd/atomicAdd.cu
+++ atomicAdd/atomicAdd.cu
@@ -1,25 +1,39 @@
 #include <stdio.h>
 
-__constant__ int increment;
+__constant__ int inc;
 __device__ int sum;
 
 __global__ void atomicAdd()
 {
-	int s = atomicAdd(&sum, increment);
+	int s = atomicAdd(&sum, inc);
 	if (threadIdx.x == 0)
 	{
-		printf("blockIdx.x = %d, c = %d\n", blockIdx.x, s);
+		printf("blockIdx.x = %d, sum = %d\n", blockIdx.x, s);
 	}
 }
 
 int main(int argc, char *argv[])
 {
-	int h_increment = 10;
+	// Initialize inc and sum.
+	int h_inc = 3;
 	int h_sum = 1;
-	cudaMemcpyToSymbol(increment, &h_increment, sizeof(int));
+
+	// Copy inc and sum from host memory to device memory synchronously.
+	cudaMemcpyToSymbol(inc, &h_inc, sizeof(int));
 	cudaMemcpyToSymbol(sum, &h_sum, sizeof(int));
+
+	// Invoke the kernel on device asynchronously.
 	atomicAdd<<<2, 2>>>();
+
+	// Wait for the device to finish.
 	cudaDeviceSynchronize();
+
+	// Copy sum from device memory to host memory synchronously.
 	cudaMemcpyFromSymbol(&h_sum, sum, sizeof(int));
+
+	// Print the result.
+	printf("sum = %d\n", h_sum);
+
+	// Cleanup.
 	cudaDeviceReset();
 }
