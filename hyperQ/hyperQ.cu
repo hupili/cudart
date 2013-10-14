e61785d168a56b7b6312d895fcef2a9d56ffe62d
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-10-14 18:50:20 +0800

    Revised hyperQ

--- hyperQ/hyperQ.cu
+++ hyperQ/hyperQ.cu
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-__global__ void hyperQ(clock_t num_clocks)
+__global__ void spin(clock_t numClocks)
 {
-	for (const clock_t threshold = clock() + num_clocks; clock() < threshold;);
+	for (const clock_t threshold = clock() + numClocks; clock() < threshold;);
 }
 
 int main(int argc, char *argv[])
 {
 	// Initialize constants.
-	const int num_milliseconds = 10;
-	const int num_kernels = 2;
-	const int num_streams = 32;
+	const int numMilliseconds = 10;
+	const int numKernels = 2;
+	const int numStreams = 32;
 
 	// Get the major and minor compute capability version numbers.
 	int major, minor;
@@ -22,11 +22,11 @@ int main(int argc, char *argv[])
 	cudaDeviceGetAttribute(&clockRate, cudaDevAttrClockRate, 0);
 
 	// Calculate the number of clocks within a certain period.
-	clock_t num_clocks = clockRate * num_milliseconds;
+	clock_t numClocks = clockRate * numMilliseconds;
 
 	// Create streams to enqueue kernels.
-	cudaStream_t *streams = (cudaStream_t*)malloc(sizeof(cudaStream_t) * num_streams);
-	for (int s = 0; s < num_streams; ++s)
+	cudaStream_t *streams = (cudaStream_t*)malloc(sizeof(cudaStream_t) * numStreams);
+	for (int s = 0; s < numStreams; ++s)
 	{
 		cudaStreamCreate(&streams[s]);
 	}
@@ -40,15 +40,15 @@ int main(int argc, char *argv[])
 	cudaEventRecord(beg, 0);
 
 	// Enqueue kernels to streams.
-	for (int s = 0; s < num_streams; ++s)
+	for (int s = 0; s < numStreams; ++s)
 	{
-		for (int k = 0; k < num_kernels; ++k)
+		for (int k = 0; k < numKernels; ++k)
 		{
 			// The 1st argument is of type dim3 and specifies the number of blocks per grid.
 			// The 2nd argument is of type dim3 and specifies the number of threads per block.
 			// The 3rd argument is of type size_t and specifiesthe number of bytes in shared memory that is dynamically allocated per block for this call in addition to the statically allocated memory. It is an optional argument which defaults to 0.
 			// The 4th argument is of type cudaStream_t and specifies the associated stream. It is an optional argument which defaults to 0.
-			hyperQ<<<1, 1, 0, streams[s]>>>(num_clocks);
+			spin<<<1, 1, 0, streams[s]>>>(numClocks);
 		}
 	}
 
@@ -62,17 +62,17 @@ int main(int argc, char *argv[])
 	float elapsed;
 	cudaEventElapsedTime(&elapsed, beg, end);
 
-	// Print the results.
-	printf("%d streams, each %d kernels, each %d ms\n", num_streams, num_kernels, num_milliseconds);
-	printf("       SM <= 1.3:%4d ms\n", num_milliseconds * num_kernels * num_streams);
-	printf("2.0 <= SM <= 3.0:%4d ms\n", num_milliseconds * (1 + (num_kernels - 1) * num_streams));
-	printf("3.5 <= SM       :%4d ms\n", num_milliseconds * num_kernels);
+	// Print the result.
+	printf("%d streams, each %d kernels, each %d ms\n", numStreams, numKernels, numMilliseconds);
+	printf("       SM <= 1.3:%4d ms\n", numMilliseconds * numKernels * numStreams);
+	printf("2.0 <= SM <= 3.0:%4d ms\n", numMilliseconds * (1 + (numKernels - 1) * numStreams));
+	printf("3.5 <= SM       :%4d ms\n", numMilliseconds * numKernels);
 	printf("       SM == %d.%d:%4d ms\n", major, minor, (int)elapsed);
 
 	// Cleanup.
 	cudaEventDestroy(end);
 	cudaEventDestroy(beg);
-	for (int s = 0; s < num_streams; ++s)
+	for (int s = 0; s < numStreams; ++s)
 	{
 		cudaStreamDestroy(streams[s]);
 	}
