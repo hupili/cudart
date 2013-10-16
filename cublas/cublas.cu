4d7d64a0eb003535f3d59f63443ad81cef8e99d1
Jacky Lee <JackyLeeHongJian@Gmail.com>
2013-10-16 14:15:15 +0800

    Revised cublas

--- cublas/cublas.cu
+++ cublas/cublas.cu
@@ -54,9 +54,6 @@ int main(int argc, char *argv[])
 		h_B[i] = rand() / (float)RAND_MAX;
 	}
 
-	// allocate host memory for the result
-	float *h_CUBLAS = (float *)malloc(mem_size_C);
-
 	// Allocate device memory
 	float *d_A, *d_B, *d_C;
 	cudaMalloc((void **)&d_A, mem_size_A);
@@ -70,29 +67,28 @@ int main(int argc, char *argv[])
 	dim3 numThreadsPerBlock(block_size, block_size);
 	dim3 numBlocksPerGrid(matrix_size.uiWC / numThreadsPerBlock.x, matrix_size.uiHC / numThreadsPerBlock.y);
 
-	// execute the kernel
+	// Initialize a cublas handle.
 	cublasHandle_t handle;
 	cublasCreate(&handle);
 
+	// cublas is column primary. Need to transpose the order.
 	const float alpha = 1.0f;
 	const float beta  = 0.0f;
 	cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, matrix_size.uiWB, matrix_size.uiHA, matrix_size.uiWA, &alpha, d_B, matrix_size.uiWB, d_A, matrix_size.uiWA, &beta, d_C, matrix_size.uiWA);
 
+	// Measure the performance of cublasSgemm over a number of iterations.
 	cudaEvent_t start, stop;
 	cudaEventCreate(&start);
 	cudaEventCreate(&stop);
 	cudaEventRecord(start, 0);
-
 	int numIterations = 30;
 	for (int j = 0; j < numIterations; j++)
 	{
-		// cublas is column primary. Need to transpose the order.
 		cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, matrix_size.uiWB, matrix_size.uiHA, matrix_size.uiWA, &alpha, d_B, matrix_size.uiWB, d_A, matrix_size.uiWA, &beta, d_C, matrix_size.uiWA);
 	}
-
 	cudaEventRecord(stop, 0);
 	cudaEventSynchronize(stop);
-	float msecTotal = 0.0f;
+	float msecTotal;
 	cudaEventElapsedTime(&msecTotal, start, stop);
 
 	// Compute and print the performance
@@ -101,27 +97,29 @@ int main(int argc, char *argv[])
 	double gigaFlops = (flopsPerMatrixMul * 1.0e-9f) / (msecPerMatrixMul / 1000.0f);
 	printf("Performance= %.2f GFlop/s, Time= %.3f msec, Size= %.0f Ops\n", gigaFlops, msecPerMatrixMul, flopsPerMatrixMul);
 
-	// copy result from device to host
-	cudaMemcpy(h_CUBLAS, d_C, mem_size_C, cudaMemcpyDeviceToHost);
+	// Copy result from device to host.
+	cudaMemcpy(h_C, d_C, mem_size_C, cudaMemcpyDeviceToHost);
+
+	// Destroy the cublas handle after use.
 	cublasDestroy(handle);
 
-	// compute reference solution
+	// Compute reference solution.
 	float *reference = (float *)malloc(mem_size_C);
 	matrixMulCPU(reference, h_A, h_B, matrix_size.uiHA, matrix_size.uiWA, matrix_size.uiWB);
 
 	// Validate the result.
-	if (!sdkCompareL2fe(reference, h_CUBLAS, size_C, 1.0e-6f))
+	if (!sdkCompareL2fe(reference, h_C, size_C, 1.0e-6f))
 	{
 		printf("error\n");
 	}
 
 	// Cleanup.
-	free(h_A);
-	free(h_B);
-	free(h_C);
 	free(reference);
-	cudaFree(d_A);
-	cudaFree(d_B);
 	cudaFree(d_C);
+	cudaFree(d_B);
+	cudaFree(d_A);
 	cudaDeviceReset();
+	free(h_C);
+	free(h_B);
+	free(h_A);
 }
