9d5fe21c77be60b925f4e92fe69fe4a58fe734ae
Pili Hu <hpl1989@gmail.com>
2014-02-11 14:37:21 +0800

    more trials of transform

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -8,23 +8,31 @@ using namespace thrust::placeholders;
 
 int main(int argc, char* argv[])
 {
-	float a = 1.0f;
+	float a = 2.0f;
 	float x[] = {1, 2, 3, 4, 7, 2};
 	float y[] = {1, 1, 1, 1, 8, 9};
 	int length = ARRAY_LENGTH(x); 
 
 	device_vector<float> X(x, x + length);
 	device_vector<float> Y(y, y + length);
+	// Other prototypes are like 
+	// http://thrust.github.io/doc/group__transformations.html#gabbda6380c902223d777cc72d3b1b9d1a
+	//transform
+	//(
+	//	X.begin(), X.end(), // Input range 1
+	//	Y.begin(),          // Input range 2
+	//	X.begin(),          // Output range
+	//	a * _1 + _2         // Lambda expression to compute ax + y
+	//);
 	transform
 	(
 		X.begin(), X.end(), // Input range 1
-		Y.begin(),          // Input range 2
-		Y.begin(),          // Output range
-		a * _1 + _2         // Lambda expression to compute ax + y
+		X.begin(),          // Output range
+		a * _1         // Lambda expression to compute ax + y
 	);
 
 	for (size_t i = 0; i < length; ++i)
 	{
-		std::cout << a << " * " << x[i] << " + " << y[i] << " = " << Y[i] << std::endl;
+		std::cout << a << " * " << x[i] << " + " << y[i] << " = " << X[i] << " ? " << Y[i] << std::endl;
 	}
 }
