d734814859541eee1497c20c63b3c7e3a2f82760
GPUDEMO <gpudemo@hpc3.cse.cuhk.edu.hk>
2014-02-11 14:27:33 +0800

    var array length

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -4,14 +4,17 @@
 using namespace thrust;
 using namespace thrust::placeholders;
 
+#define ARRAY_LENGTH(x) sizeof(x) / sizeof(x[0])
+
 int main(int argc, char* argv[])
 {
-	float a = 2.0f;
-	float x[4] = {1, 2, 3, 4};
-	float y[4] = {1, 1, 1, 1};
+	float a = 1.0f;
+	float x[] = {1, 2, 3, 4, 7, 2};
+	float y[] = {1, 1, 1, 1, 8, 9};
+	int length = ARRAY_LENGTH(x); 
 
-	device_vector<float> X(x, x + 4);
-	device_vector<float> Y(y, y + 4);
+	device_vector<float> X(x, x + length);
+	device_vector<float> Y(y, y + length);
 	transform
 	(
 		X.begin(), X.end(), // Input range 1
@@ -20,7 +23,7 @@ int main(int argc, char* argv[])
 		a * _1 + _2         // Lambda expression to compute ax + y
 	);
 
-	for (size_t i = 0; i < 4; ++i)
+	for (size_t i = 0; i < length; ++i)
 	{
 		std::cout << a << " * " << x[i] << " + " << y[i] << " = " << Y[i] << std::endl;
 	}
